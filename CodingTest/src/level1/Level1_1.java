a9cd08ee858d1bdb7466bea9e1e6fc91fc807488
heominho <heominho@DESKTOP-8H87K82>
2020-09-22 00:46:56 +0900

    test git hub

---
+++ CodingTest/src/level1/Level1_1.java
@@ -0,0 +1,11 @@
+package level1;
+
+public class Level1_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String test1 = "Git Hub Test";
+		System.out.println("Hello World"+test1);
+	}
+
+}
